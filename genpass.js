373b773471f420ec3d7778fa84d8302cac0a7d39
newnakashima <newnakashima2014@gmail.com>
2018-09-14 20:41:14 +0900

    大文字が一文字紛れ込むようになった

--- genpass.js
+++ genpass.js
@@ -1,5 +1,14 @@
-class Password(min = 12, max = 16, sign = 1, num = 1, cap = 1) {
-    constructor() {
+var n = require("./util/nuckle.js");
+
+const CODE = {
+    ALPHA_START: 97,
+    ALPHA_END: 122,
+    CAP_ALPHA_START: 65,
+    CAP_ALPHA_END: 90
+}
+
+class Password {
+    constructor(min = 12, max = 16, sign = 1, num = 1, cap = 1) {
         if (min > max) {
             throw new Error('min は max 以下でなくてはいけません！');
         }
@@ -14,9 +23,36 @@ class Password(min = 12, max = 16, sign = 1, num = 1, cap = 1) {
 
     gen() {
         if (this.min != this.max) {
-            this.length = Math.floor(Math.random() * (max + 1 - min)) + min;
+            this.length = Math.floor(Math.random() * (this.max + 1 - this.min)) + this.min;
         } 
+        const alpha_length = this.length - (this.sign + this.num + this.cap);
+        const alpha_letters = this.create_alpha_letters(alpha_length);
+        const cap_letters = this.create_cap_letters(this.cap);
+        const shuffled = n.array_shuffle(alpha_letters.concat(cap_letters));
+        console.log(shuffled.join(''));
+    }
 
-        let s_leters = [];
+    create_alpha_letters(length) {
+        return n.range(length).map(() => this.alpha_char());
+    }
+
+    create_cap_letters(length) {
+        return n.range(length).map(() => this.cap_char());
+    }
+
+    random_char(start, end) {
+        const code = Math.floor(Math.random() * (end + 1 - start)) + start;
+        return String.fromCharCode(code);
+    }
+
+    alpha_char() {
+        return this.random_char(CODE.ALPHA_START, CODE.ALPHA_END);
+    }
+
+    cap_char() {
+        return this.random_char(CODE.CAP_ALPHA_START, CODE.CAP_ALPHA_END);
     }
 }
+
+let p = new Password();
+p.gen();
---
+++ util/nuckle.js
@@ -0,0 +1,34 @@
+module.exports = {
+    /**
+     * method
+     * @param {Number} max Max value of the range.
+     * @param {Number} min Min value of the range.
+     * @parma {Number} step Step value of the range.
+     * @return {Array<Number>} Number Array of range stepped by step.
+     */
+    range: function (max, min = 1, step = 1) {
+        if (max == min) {
+            return [max];
+        }
+
+        if (max < min) {
+            throw new Error('max must be larger than min');
+        }
+
+        let range = [];
+        for (let i = min; i <= max; i += step) {
+            range.push(i);
+        }
+        return range;
+    },
+    array_shuffle: function (input_array) {
+        for (let i = input_array.length - 1; i > 0; i--) {
+            let r = Math.floor(Math.random() * (i + 1));
+            let tmp = input_array[i];
+            input_array[i] = input_array[r];
+            input_array[r] = tmp;
+            return input_array;
+        }
+    }
+};
+
