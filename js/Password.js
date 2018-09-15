cc9e60c9bc9e0b84afff9b359dbcd5024f53e569
newnakashima <newnakashima2014@gmail.com>
2018-09-16 07:54:44 +0900

    バグを修正した

--- js/Password.js
+++ js/Password.js
@@ -1,12 +1,9 @@
 import n from './util/nuckle';
 
 const CODE = {
-    ALPHA_START:     97,
-    ALPHA_END:       122,
-    CAP_ALPHA_START: 65,
-    CAP_ALPHA_END:   90,
-    NUM_START:       48,
-    NUM_END:         57
+    ALPHA: {start: 97, end: 122},
+    CAP:   {start: 65, end:  92},
+    NUM:   {start: 48, end:  57}
 }
 
 class Password {
@@ -35,11 +32,21 @@ class Password {
             const cap_letters = this.create_letters(this.cap, () => this.cap_char());
             const sign_letters = this.create_letters(this.sign, () => this.sign_char());
             const num_letters = this.create_letters(this.num, () => this.num_char());
-            const shuffled = n.array_shuffle(alpha_letters.concat(cap_letters, sign_letters, num_letters));
-            return shuffled.join('');
+            const cat = alpha_letters.concat(cap_letters, sign_letters, num_letters);
+            const shuffled = n.array_shuffle(cat);
+            return {
+                length:  this.length,
+                text:    shuffled.join(''),
+                success: true
+            };
         } catch (e) {
             console.error('failed');
-            return "gen failed";
+            // return "gen failed";
+            return {
+                length:  -1,
+                text:    '<span style="color:red">パスワードの生成に失敗しました。最小文字数か最大文字数が小さすぎる可能性があります。</span>',
+                success: false
+            };
         }
     }
 
@@ -47,16 +54,16 @@ class Password {
         return n.range(length).map(() => func());
     }
 
-    random_index(start, end) {
-        return Math.floor(Math.random() * (end + 1 - start)) + start;
+    random_index(type) {
+        return Math.floor(Math.random() * (type.end + 1 - type.start)) + type.start;
     }
 
     alpha_char() {
-        return String.fromCharCode(this.random_index(CODE.ALPHA_START, CODE.ALPHA_END));
+        return String.fromCharCode(this.random_index(CODE.ALPHA));
     }
 
     cap_char() {
-        return String.fromCharCode(this.random_index(CODE.CAP_ALPHA_START, CODE.CAP_ALPHA_END));
+        return String.fromCharCode(this.random_index(CODE.CAP));
     }
 
     sign_char() {
@@ -66,17 +73,29 @@ class Password {
             {start: 91,  end:   96 },
             {start: 123, end:   126}
         ];
-        let signs = [];
+        let signs = ["<"];
         parts.forEach(e => {
             for(let i = e.start; i <= e.end; i++) {
-                signs.push(String.fromCharCode(i));
+                // signs.push(String.fromCharCode(i));
             }
         });
-        return signs[this.random_index(0, signs.length -1)];
+        let picked = signs[this.random_index({start: 0, end: signs.length - 1})];
+        return this.escape_char(picked);
+    }
+
+    escape_char(input_char) {
+        const escape_list = {
+            "<": "&lt;",
+            ">": "&gt;"
+        };
+        if (input_char in escape_list) {
+            return escape_list[input_char];
+        }
+        return input_char;
     }
 
     num_char() {
-        return String.fromCharCode(this.random_index(CODE.NUM_START, CODE.NUM_END));
+        return String.fromCharCode(this.random_index(CODE.NUM));
     }
 }
 export default Password;
